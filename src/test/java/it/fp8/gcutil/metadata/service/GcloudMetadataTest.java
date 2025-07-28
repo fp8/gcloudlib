225f451916544e50dc1bfa6b3d1a7470088f2179
Guido Giordano <guido.giordano@farport.co>
2025-07-28 10:37:53 +0000

    refactor

--- src/test/java/it/fp8/gcutil/metadata/service/GcloudMetadataTest.java
+++ src/test/java/it/fp8/gcutil/metadata/service/GcloudMetadataTest.java
@@ -1,6 +1,5 @@
 package it.fp8.gcutil.metadata.service;
 
-import it.fp8.gcutil.metadata.service.GcloudMetadata;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -11,7 +10,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class GcloudMetadataTest {
 
     private static final String BASE_URL = "https://nginx-839315814860.europe-west1.run.app/computeMetadata/v1";
-    private static final String ID_TOKEN = System.getenv("ID_TOKEN");
     @Test
     public void testGetProjectId() throws IOException, InterruptedException {
         var service = new GcloudMetadata("https://nginx-839315814860.europe-west1.run.app/computeMetadata/v1");
